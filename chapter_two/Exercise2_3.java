a3742e0478a4953a1050c859395ed7641280df0f
Ryoung27 <RRYoung89@gmail.com>
2020-08-11 09:42:42 -0500

    exercise 2.4 pound to kilograms

--- chapter_two/Exercise2_3.java
+++ chapter_two/Exercise2_3.java
@@ -5,7 +5,6 @@ public class Exercise2_3{
         final double FEET_TO_METER = 0.305; //Declare a constant
         Scanner input = new Scanner(System.in);
 
-
         //Prompt the user for a distance in feet.
         System.out.print("Please enter a distance in feet: ");
         double feet = input.nextDouble();
