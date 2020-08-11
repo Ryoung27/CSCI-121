c3fdbbed8aef0a3f0f27f1d2e99fc032b2087685
Ryoung27 <RRYoung89@gmail.com>
2020-08-11 09:39:59 -0500

    exercise 2.3 feet to meters

--- chapter_two/Exercise2_3.java
+++ chapter_two/Exercise2_3.java
@@ -2,7 +2,18 @@ import java.util.Scanner;
 
 public class Exercise2_3{
     public static void main(String[] args){
-        System.out.println("Test");
+        final double FEET_TO_METER = 0.305; //Declare a constant
         Scanner input = new Scanner(System.in);
+
+
+        //Prompt the user for a distance in feet.
+        System.out.print("Please enter a distance in feet: ");
+        double feet = input.nextDouble();
+
+        double answer = feet * FEET_TO_METER;
+
+        System.out.println(feet + " is " + answer + " meters");
+
+
     }
 }
