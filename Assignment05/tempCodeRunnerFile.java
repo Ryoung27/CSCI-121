c8b729f7fa0609bd41f847f860dbb9d9965f75b9
Ryoung27 <RRYoung89@gmail.com>
2020-09-23 13:27:43 -0500

    project 5

---
+++ Assignment05/tempCodeRunnerFile.java
@@ -0,0 +1,16 @@
+
+        // System.out.println("Please enter your principle, term, and interest rate: ");
+        // System.out.print("Enter the principle to borrow: ");
+        // principle = input.nextInt();
+
+        // System.out.print("Enter the interest rate in decimal: ");
+        // interestRate = input.nextDouble();
+
+        // System.out.print("Enter the term (in years): ");
+        // term = input.nextInt();
+        // totalTermInMonths = term*12;
+
+        // totalOwed = principle + (principle * interestRate);
+
+        // // System.out.println(monthlyPayment);
+        // System.out.println("Month       " + "Payment      " + "Total Interest       " + "Balance       ");
